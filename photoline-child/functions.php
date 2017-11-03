a40b86458a2ea2fa457dd15f0d125416b7f85bff
aleks <aleks.kozhukhov@yandex.ru>
2017-11-03 15:52:13 +0300

    comment function.php

--- photoline-child/functions.php
+++ photoline-child/functions.php
@@ -8,7 +8,7 @@
 /*-----------------------------------------------------------------------------------*/
 
 // remove fonts css parent theme
-function photoline_child_remove_parent_styles() {
+/*function photoline_child_remove_parent_styles() {
 	wp_dequeue_style( 'photoline-fonts' );
 }
 add_action( 'wp_enqueue_scripts', 'photoline_child_remove_parent_styles', 11 );
@@ -17,32 +17,32 @@ function photoline_child_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'photoline-child-fonts', photoline_child_fonts_url(), array(), null );
 }
-add_action( 'wp_enqueue_scripts', 'photoline_child_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'photoline_child_enqueue_styles' );*/
 
 /**
  * Register Google fonts for Theme
  */
-if ( ! function_exists( 'photoline_child_fonts_url' ) ) :
-function photoline_child_fonts_url() {
-    $fonts_url = '';
- 
-    $lora = _x( 'on', 'Lora font: on or off', 'photoline_child' ); // Lora font replace your
- 
-    if ( 'off' !== $lora ) { // $lora replace your
-        $font_families = array();
- 
-        if ( 'off' !== $lora ) {
-            $font_families[] = 'Lora:400,700,400italic'; // Lora font replace your
-        }
- 
-        $query_args = array(
-            'family' => urlencode( implode( '|', $font_families ) ),
-            'subset' => urlencode( 'latin' ),
-        );
- 
-        $fonts_url = add_query_arg( $query_args, '//fonts.googleapis.com/css' );
-    }
- 
-    return $fonts_url;
-}
-endif;
+//if ( ! function_exists( 'photoline_child_fonts_url' ) ) :
+//function photoline_child_fonts_url() {
+    //$fonts_url = '';
+
+    //$lora = _x( 'on', 'Lora font: on or off', 'photoline_child' ); // Lora font replace your
+
+    //if ( 'off' !== $lora ) { // $lora replace your
+       // $font_families = array();
+
+        //if ( 'off' !== $lora ) {
+           // $font_families[] = 'Lora:400,700,400italic'; // Lora font replace your
+       // }
+
+       // $query_args = array(
+           // 'family' => urlencode( implode( '|', $font_families ) ),
+            //'subset' => urlencode( 'latin' ),
+        //);
+
+       // $fonts_url = add_query_arg( $query_args, '//fonts.googleapis.com/css' );
+    //}
+
+    //return $fonts_url;
+//}
+//endif;
