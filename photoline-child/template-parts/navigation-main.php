6d5910c024ceeda9fa052d2d3e4f64b10387476e
aleks <aleks.kozhukhov@yandex.ru>
2017-11-15 16:05:44 +0300

    move search to top menu version 1

---
+++ photoline-child/template-parts/navigation-main.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package Photoline
+ */
+?>
+
+<?php 
+	if ( has_nav_menu('primary') ) { ?>
+
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<h1 class="menu-toggle"><span class="screen-reader-text"><?php _e( 'Menu', 'photoline' ); ?></span></h1>
+<?php
+$form = '
+	<form role="search" method="get" class="search-form" action="'. esc_url( home_url('/') ).'">
+	<label>
+		<input type="search" class="search-field" placeholder="Search" value="" name="s">
+	</label>
+	<input type="submit" class="search-submit" value="">
+</form>';
+		wp_nav_menu(
+		array(
+			'theme_location' => 'primary',
+			'menu_class' => 'nav-menu',
+			'container'       => 'div',
+			'container_class' => 'menu-main',
+            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul><div class="search-top">'.$form.'</div>'
+		) ); ?>
+
+		</nav>
+<?php
+	} ?>
